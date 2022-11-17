e057e13dfd582bb679586ca9bd2422ed93ff9a3f
Paczek_W_Masle <paczek_w_masle@github.com>
2022-11-17 19:14:37 +0100

    Initial app setup

---
+++ src/main/kotlin/com/michaelwhyte/rowerowyrownik/RowerowyrownikSquaresOverlayApplication.kt
@@ -0,0 +1,11 @@
+package com.michaelwhyte.rowerowyrownik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RowerowyrownikSquaresOverlayApplication
+
+fun main(args: Array<String>) {
+	runApplication<RowerowyrownikSquaresOverlayApplication>(*args)
+}
